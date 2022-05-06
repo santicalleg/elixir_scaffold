a16e2a83a80fb19b88e3b3bdb3a959248c856d9e
Santiago Calle Gomez <sancalle@Santiagos-MacBook-Pro-2.local>
2022-05-06 09:46:45 -0500

    Creates project

---
+++ lib/mix/tasks/hello.ex
@@ -0,0 +1,17 @@
+defmodule Hello do
+
+  alias ElixirStructureManager.Core.ApplyTemplates
+  require Logger
+
+  @structure_path "./lib/create_structure/parameters/create_structure.json"
+
+  def run(application_name) do
+    with {:ok, atom_name, module_name} <- ApplyTemplates.manage_application_name(application_name),
+         template <- ApplyTemplates.load_template_file(@structure_path),
+         {:ok, variable_list} <- ApplyTemplates.create_variables_list(atom_name, module_name) do
+      ApplyTemplates.create_folder(template, atom_name, variable_list)
+    else
+      error -> Logger.error("Ocurrio un error creando la estructura: #{inspect(error)}")
+    end
+  end
+end
---
+++ mix.exs
@@ -0,0 +1,46 @@
+defmodule ElixirStructureManager.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixir_structure_manager,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
+      description: description(),
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  defp description() do
+    """
+    Library for manipulating and validating IBAN account numbers.
+    """
+  end
+  defp package() do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Juan Esteban"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/juanes1a/elixir_structure_manager"}
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:poison, "~> 4.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:mock, "~> 0.3.7", only: :test}
+    ]
+  end
+end
