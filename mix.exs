19de61c43ea8c99de83b15edacb48cf2bc95f65e
Santiago Calle Gomez <sancalle@sancalle.local>
2022-09-15 11:28:08 -0500

    Modify version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ScaffoldCa.MixProject do
   def project do
     [
       app: :scaffold_ca,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
